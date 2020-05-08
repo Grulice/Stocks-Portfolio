62031e951bbb08b7c42c4e1c866afa21b5e8b56e
Javid Bunyadzade <j.bunyadzade@gmail.com>
2020-05-08 17:01:21 +0400

    Updates

---
+++ precache-manifest.ec43abbf9b4254b715936b524743eabf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f283bfb9ac76cec9a4bf084df43127",
+    "url": "/Stocks-Portfolio/index.html"
+  },
+  {
+    "revision": "b851d3525ef4bbd9e701",
+    "url": "/Stocks-Portfolio/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "e3749cd78b9ae886ade7",
+    "url": "/Stocks-Portfolio/static/css/main.acdff8d4.chunk.css"
+  },
+  {
+    "revision": "b851d3525ef4bbd9e701",
+    "url": "/Stocks-Portfolio/static/js/2.5be0ed04.chunk.js"
+  },
+  {
+    "revision": "1f343f3b00bd42841cd23cdb6e2b08ec",
+    "url": "/Stocks-Portfolio/static/js/2.5be0ed04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3749cd78b9ae886ade7",
+    "url": "/Stocks-Portfolio/static/js/main.ee77a00b.chunk.js"
+  },
+  {
+    "revision": "159d083a2159df5f736b",
+    "url": "/Stocks-Portfolio/static/js/runtime-main.220c65fa.js"
+  },
+  {
+    "revision": "bece37656bf72a86e98e33107f755091",
+    "url": "/Stocks-Portfolio/static/media/Logo.bece3765.svg"
+  },
+  {
+    "revision": "b2e47c0a560ae181b3fea82390ef0573",
+    "url": "/Stocks-Portfolio/static/media/arrow.b2e47c0a.svg"
+  },
+  {
+    "revision": "addcea543b62db58840f4f3a520ce0f2",
+    "url": "/Stocks-Portfolio/static/media/lens.addcea54.svg"
+  }
+]);
